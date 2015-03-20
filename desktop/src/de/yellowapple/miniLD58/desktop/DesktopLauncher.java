f6be2570a16810646c850735dd51647f6b357040
Adrian Böhm <adrian.boehm22@googlemail.com>
2015-03-20 23:14:36 +0100

    .

--- desktop/src/de/yellowapple/miniLD58/desktop/DesktopLauncher.java
+++ desktop/src/de/yellowapple/miniLD58/desktop/DesktopLauncher.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import de.yellowapple.miniLD58.GameMain;
 
 public class DesktopLauncher {
-    
+
     public static void main(String[] arg) {
 	LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
