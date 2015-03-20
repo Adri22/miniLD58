f687dafc6b8276ee6c070e0d90c88ad056fd7295
Adrian Böhm <adrian.boehm22@googlemail.com>
2015-03-20 22:34:07 +0100

    init

---
+++ desktop/src/de/yellowapple/miniLD58/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.yellowapple.miniLD58.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.yellowapple.miniLD58.GameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameMain(), config);
+	}
+}
---
+++ html/src/de/yellowapple/miniLD58/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package de.yellowapple.miniLD58.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import de.yellowapple.miniLD58.GameMain;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new GameMain();
+        }
+}
