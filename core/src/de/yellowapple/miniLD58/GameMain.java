9946ee156319c1f50b4b4687923965ba3bac0182
Adrian Böhm <adrian.boehm22@googlemail.com>
2015-03-20 22:49:49 +0100

    test

--- core/src/de/yellowapple/miniLD58/GameMain.java
+++ core/src/de/yellowapple/miniLD58/GameMain.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+// test
+
+
 public class GameMain extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
